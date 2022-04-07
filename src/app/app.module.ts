465111f876b6f3e561aa3c59894d8abaf61303ea
Amanda <amandakehldefarias@hotmail.com>
2022-04-06 21:56:46 -0300

    improving EntryFormComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -14,6 +15,7 @@ import { InMemoryDatabase } from './pages/in-memory-database';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase) // Para usar back-end externo, precisa remover essa linha e apagar o arquivo do in memory 
